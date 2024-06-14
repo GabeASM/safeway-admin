9f6913022b999532756f2f4a74bfe4f71e6a0230
DaniNaranja <d.paredes04@ufromail.cl>
2024-06-13 22:54:52 -0400

    fix: rollback de daisyui

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+import { defineComponent } from 'vue';
+
+import NavBar from '@/components/NavBar.vue';
+
+defineComponent({
+  components: {
+    NavBar,
+  },
+});
 
 </script>
 
 <template>
+  <NavBar></NavBar>
 
   <div>
     <div class="font-bold text-sky-400">Texto de relleno. Tailwind funcionando</div>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="navbar bg-neutral-950">
+  <div class="flex-1">
+    <router-link to="/">
+      <img src="../assets/logo.png" alt="" class="h-24 p-2">
+    </router-link>
+    
+  </div>
+  <div class="flex-none gap-4">
+
+
+    
+    
+  </div>
+</div>
+
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+    name: 'NavBar',
+})
+
+</script>
+
+<style scoped>
+
+
+</style>
